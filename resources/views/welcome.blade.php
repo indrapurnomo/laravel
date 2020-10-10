a1ecabedec6d1604d1ad6e74814861046d8a0a0e
indrapurnomo <indra.purnomo@mail.ugm.ac.id>
2020-10-10 10:19:08 +0700

    edit judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel indra Laravel
+                    Laravel indra Laravel coba-coba
                 </div>
 
                 <div class="links">
